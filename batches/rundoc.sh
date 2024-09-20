af187c1cb841d80b4d5317ffcf541914155070a6
Randall Ratsch <rratsch@gmail.com>
2024-09-20 09:51:49 -0400

    Folder restructure

---
+++ batches/rundoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+options="doc"
+runablefolder=../../vxlisp/dist
+# runablefolder=../../vxlisp/src
+currentfolder=$PWD
+srcfolder=%currentfolder%/../src
+runable=vxlisp_linux64
+# runable=vxlisp_mac64
+# runable="go run ."
+pipe=
+# pipe=> %currentfolder%/run.log
+cd $runablefolder
+$runable $options :path $srcfolder $pipe
+cd $currentfolder
